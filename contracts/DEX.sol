aaddc3a7d6ffb9aa25c22b3300fe009d59f4d7ff
guibvieira <guibvieira@gmail.com>
2021-05-09 13:21:51 +0100

    add market order to DEX contract, add unit tests for market orders

--- contracts/DEX.sol
+++ contracts/DEX.sol
@@ -19,6 +19,7 @@ contract DEX is Wallet {
         bytes32 ticker;
         uint amount;
         uint price;
+        uint filled;
     }
 
     mapping( bytes32 => mapping(uint => Order[])) public orderBook;
@@ -35,59 +36,99 @@ contract DEX is Wallet {
             require(balances[msg.sender][ticker] >= amount);
         }
 
-        Order[] storage order = orderBook[ticker][uint(side)];
+        Order[] storage orders = orderBook[ticker][uint(side)];
 
-        order.push(Order(nextOrderId,msg.sender, side, ticker, amount, price));
-        
-        // if(side == Side.BUY){
-        //     Order memory tempOrder;
-        //     for(uint i=order.length-1; i=1; i--){
-        //         if (order[i].price >= order[i-1].price){
-        //             tempOrder = order[i-1];
-        //             order[i-1] = order[i];
-        //             order[i] = tempOrder;
-        //         }
-        //     }
-        // } else if (side == Side.SELL){
-        //     Order memory tempOrder;
-        //     for(uint i=order.length-1; i=1; i--){
-        //         if (order[i].price <= order[i-1].price){
-        //             tempOrder = order[i-1];
-        //             order[i-1] = order[i];
-        //             order[i] = tempOrder;
-        //         }
-        //     }
-        // }
+        orders.push(Order(nextOrderId,msg.sender, side, ticker, amount, price, 0));
 
-        uint i = order.length > 0 ? order.length - 1 : 0;
+        uint i = orders.length > 0 ? orders.length - 1 : 0;
 
         //Bubble sort
         if(side == Side.BUY){
-            Order memory tempOrder;
-            while (i>0){
-                if (order[i-1].price > order[i].price){
+            while(i > 0){
+                if(orders[i - 1].price > orders[i].price) {
                     break;
                 }
-                    tempOrder = order[i-1];
-                    order[i-1] = order[i];
-                    order[i] = tempOrder;
-                    i--;
+                Order memory orderToMove = orders[i - 1];
+                orders[i - 1] = orders[i];
+                orders[i] = orderToMove;
+                i--;
             }
         } else if (side == Side.SELL){
-            Order memory tempOrder;
-            while (i>0){
-                if (order[i-1].price < order[i].price){
-                    break;
+             while(i > 0){
+                if(orders[i - 1].price < orders[i].price) {
+                    break;   
                 }
-                    tempOrder = order[i-1];
-                    order[i-1] = order[i];
-                    order[i] = tempOrder;
-                    i--;
+                Order memory orderToMove = orders[i - 1];
+                orders[i - 1] = orders[i];
+                orders[i] = orderToMove;
+                i--;
             }
         }
         nextOrderId++;
 
     }
 
-    // createMarketOrder(0, web3.utils.fromUtf8("LINK"), 10)/s
+    function createMarketOrder(Side side, bytes32 ticker, uint amount) public{
+        if(side == Side.SELL){
+            require(balances[msg.sender][ticker] >= amount, "Insuffient balance");
+        }
+        
+        uint orderBookSide;
+        if(side == Side.BUY){
+            orderBookSide = 1;
+        }
+        else{
+            orderBookSide = 0;
+        }
+        Order[] storage orders = orderBook[ticker][orderBookSide];
+
+        uint totalFilled = 0;
+
+        for (uint256 i = 0; i < orders.length && totalFilled < amount; i++) {
+            uint leftToFill = amount.sub(totalFilled);
+            uint availableToFill = orders[i].amount.sub(orders[i].filled);
+            uint filled = 0;
+            if(availableToFill > leftToFill){
+                filled = leftToFill; //Fill the entire market order
+            }
+            else{ 
+                filled = availableToFill; //Fill as much as is available in order[i]
+            }
+
+            totalFilled = totalFilled.add(filled);
+            orders[i].filled = orders[i].filled.add(filled);
+            uint cost = filled.mul(orders[i].price);
+
+            if(side == Side.BUY){
+                //Verify that the buyer has enough ETH to cover the purchase (require)
+                require(balances[msg.sender]["ETH"] >= cost);
+                //msg.sender is the buyer
+                balances[msg.sender][ticker] = balances[msg.sender][ticker].add(filled);
+                balances[msg.sender]["ETH"] = balances[msg.sender]["ETH"].sub(cost);
+                
+                balances[orders[i].trader][ticker] = balances[orders[i].trader][ticker].sub(filled);
+                balances[orders[i].trader]["ETH"] = balances[orders[i].trader]["ETH"].add(cost);
+            }
+            else if(side == Side.SELL){
+                //Msg.sender is the seller
+                balances[msg.sender][ticker] = balances[msg.sender][ticker].sub(filled);
+                balances[msg.sender]["ETH"] = balances[msg.sender]["ETH"].add(cost);
+                
+                balances[orders[i].trader][ticker] = balances[orders[i].trader][ticker].add(filled);
+                balances[orders[i].trader]["ETH"] = balances[orders[i].trader]["ETH"].sub(cost);
+            }
+            
+        }
+            //Remove 100% filled orders from the orderbook
+        while(orders.length > 0 && orders[0].filled == orders[0].amount){
+            //Remove the top element in the orders array by overwriting every element
+            // with the next element in the order list
+            for (uint256 i = 0; i < orders.length - 1; i++) {
+                orders[i] = orders[i + 1];
+            }
+            orders.pop();
+        }
+        
+    }
 }
+
--- test/marketOrderTest.js
+++ test/marketOrderTest.js
@@ -4,7 +4,7 @@ const truffleAssert = require("truffle-assertions");
 
 contract("DEX", accounts => {
 
-    it("hould throw an error when creating a sell market order without adequate token balance", async () => {
+    it("Should throw an error when creating a sell market order without adequate token balance", async () => {
         let dex = await Dex.deployed();
 
         let balance = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
@@ -21,7 +21,7 @@ contract("DEX", accounts => {
         assert.equal(balance.toNumber(), 0, "Initial Link balance is not 0");
         
         await truffleAssert.reverts(
-            dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 10)
+            dex.createLimitOrder(0, web3.utils.fromUtf8("LINK"), 10, 10)
         )
     })
 
@@ -42,20 +42,21 @@ contract("DEX", accounts => {
         let dex = await Dex.deployed();
         let link = await Link.deployed();
 
-        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 0);
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 1);
         assert(orderbook.length == 0, "Orderbook isn't empty");
 
         await dex.addToken(web3.utils.fromUtf8("LINK"), link.address);
+        await dex.depositEth({value: 10000, from: accounts[0]});
 
         //Send Link token to accounts 1,2,3 from account0
-        await link.transfer(accounts[0], 50);
-        await link.transfer(accounts[1], 50);
-        await link.transfer(accounts[2], 50);
+        await link.transfer(accounts[1], 150);
+        await link.transfer(accounts[2], 150);
+        await link.transfer(accounts[3], 150);
 
         //Approve DEX for accounts 1,2,3
-        await link.approve(accounts[1], 50);
-        await link.approve(accounts[2], 50);
-        await link.approve(accounts[3], 50);
+        await link.approve(dex.address, 500, {from: accounts[1]});
+        await link.approve(dex.address, 500, {from: accounts[2]});
+        await link.approve(dex.address, 500, {from: accounts[3]});
 
         //Deposit link into DEX for accounts 1,2,3
         await dex.deposit(50, web3.utils.fromUtf8("LINK"), {from: accounts[1]});
@@ -63,9 +64,9 @@ contract("DEX", accounts => {
         await dex.deposit(50, web3.utils.fromUtf8("LINK"), {from: accounts[3]});
 
         //Fill up the sell order book
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 300);
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 200);
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 100);
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 300, {from: accounts[1]});
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 200, {from: accounts[2]});
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 100, {from: accounts[3]});
 
         //Create market order that should fill 2/3 of the order book
         await dex.createMarketOrder(0, web3.utils.fromUtf8("LINK"), 10);
@@ -76,25 +77,26 @@ contract("DEX", accounts => {
     });
 
     it("Market orders should be filled until the order book is empty", async () => {
-        let dex = await Dex.deployed();
+        let dex = await Dex.deployed()
 
-        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 1);
-        assert(orderbook.length == 1, "Sell side orderbook should have 1 order left");
+        let orderbook = await dex.getOrderBook(web3.utils.fromUtf8("LINK"), 1); //Get sell side orderbook
+        assert(orderbook.length == 1, "Sell side Orderbook should have 1 order left");
 
         //Fill up the sell order book again
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 400);
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 500);
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 400, {from: accounts[1]})
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 5, 500, {from: accounts[2]})
 
-        //Check balance of buyer before making a purchase
-        let balanceBefore = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+        //check buyer link balance before link purchase
+        let balanceBefore = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"))
 
-        //Create order that would fill more than the entire order book (15 link)
+        //Create market order that could fill more than the entire order book (15 link)
         await dex.createMarketOrder(0, web3.utils.fromUtf8("LINK"), 50);
 
-        //Check balance of buyer after making a purchase
-        let balanceAfter = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+        //check buyer link balance after link purchase
+        let balanceAfter = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"))
 
-        assert.equal(balanceBefore + 15, balanceAfter);
+        //Buyer should have 15 more link after, even though order was for 50. 
+        assert.equal(balanceBefore.toNumber() + 15, balanceAfter.toNumber());
     })
 
     it("The eth balance of the buyer should decrease with the filled amount", async () => {
@@ -103,14 +105,14 @@ contract("DEX", accounts => {
 
         //Seller deposits link and creates sell order or 1 link for 300 wei
         await link.approve(dex.address, 500, {from: accounts[1]});
-        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 1, 300);
+        await dex.createLimitOrder(1, web3.utils.fromUtf8("LINK"), 1, 300, {from: accounts[1]});
 
         //Check buyer before and after eth balance
         let balanceBefore = await dex.balances(accounts[0], web3.utils.fromUtf8("ETH"));
         await dex.createMarketOrder(0, web3.utils.fromUtf8("LINK"), 1);
-        let balanceAfter = await dex.balances(accounts[0], web3.utils.fromUtf8("LINK"));
+        let balanceAfter = await dex.balances(accounts[0], web3.utils.fromUtf8("ETH"));
 
-        assert.equal(balanceBefore - 300, balanceAfter);
+        assert.equal(balanceBefore.toNumber() - 300, balanceAfter.toNumber());
     })
 
     //The token balances of the limit order sellers should decrease with the filled amounts.
@@ -144,7 +146,7 @@ contract("DEX", accounts => {
     })
 
     //Filled limit orders should be removed from the orderbook
-    xit("Filled limit orders should be removed from the orderbook", async () => {
+    it("Filled limit orders should be removed from the orderbook", async () => {
         let dex = await Dex.deployed()
         let link = await Link.deployed()
         await dex.addToken(web3.utils.fromUtf8("LINK"), link.address)
